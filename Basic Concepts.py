91567077f445eb6d1a939b6d5bc763f652183fec
ash.23@pm.me <ash.23@pm.me>
2019-02-22 22:49:34 +0200

    Change main.py to Basic Concepts.py

---
+++ Basic Concepts.py
@@ -0,0 +1,12 @@
+from __future__ import division
+print ('\n\n\n   ====  Welcome to The Recapping Calculator ==== \n')
+
+first_var = input("# Enter the first var please: ")
+second_var = input("# Enter the second var please: ")
+
+print("\n"+"The two vars added: " + str(first_var+second_var)+"\n")
+print("The two vars multiplied: " + str(first_var*second_var)+"\n")
+print("The two vars divided: " + str(float(first_var/second_var))+"\n")
+print("The two vars exponented: " + str(first_var**second_var)+"\n")
+print("The remainder of first var divided by the second: " + str(first_var%second_var)+"\n")
+print("The floor division for first and second vars: " + str(first_var//second_var)+"\n")
